307dfbf4af4568988b2fcd9a358abdb9f35a6735
Mark Fee <git@markfee.com>
2022-04-24 16:05:15 +0100

    change to check workflow

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Index = () => {
   return <div>
-    <h1>Hello From Next JS</h1>
+    <h1>Hello From Next JS Changed to test workflow</h1>
     <p>Description of the page</p>
   </div>
 }
