fd3c7f0a088a2854becb0b7a9c740d440009690a
Mark Fee <git@markfee.com>
2022-05-01 12:57:24 +0100

    changed the next.config base path to root

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/src',
-  assetPrefix: '/src/'
+  basePath: '',
+  assetPrefix: '/'
 }
 
 module.exports = nextConfig
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 
 const Index = () => {
   return <div>
-    <h1>Hello From Next JS Changed to test workflow #2</h1>
-    <p>Description of the page</p>
+    <h1>Karens Cottages</h1>
+    <p>Welcome to Karens Cottages</p>
   </div>
 }
 
