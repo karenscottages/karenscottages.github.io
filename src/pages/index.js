df15805aa15b131a88fcd9fabee704b97773dcb1
Mark Fee <git@markfee.com>
2022-05-03 00:25:11 +0100

    basic bulma styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,55 @@
 import React from "react"
+import Head from "next/head";
+import { Card } from '../components/atoms/cards'
 
 const Index = () => {
   return <div>
-    <h1>Karens Cottages</h1>
-    <p>Welcome to Karens Cottages</p>
+  <Head>
+    <title>Karen's Cottages</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+  </Head>
+
+  <section class="hero is-small is-primary">
+    <div class="hero-body">
+      <p class="title"> Karen's Cottages </p>
+      <p class="subtitle"> Beautiful Holiday Homes in Pickering, North Yorkshire. </p>
+    </div>
+  </section>
+
+  <section className = "section">
+    <Card title="New Meadows">
+      <Card.Content>
+        <div className='block'>
+          New Meadows is a single storey cottage, ideal for couples but will sleep up to 4. It offers a double bedroom along with a living room with a double sofa bed.
+          It is well furnished with beautiful decor and has everything provided including it's own patio in a large secluded garden with off road parking.        
+        </div>
+        <div className='block'>
+          New Meadows is located in a quiet part of Pickering just 10 mins walk from the town centre. 
+        </div>
+        <div className='block'>
+          The cottage is located on a quiet road.
+        </div>
+      </Card.Content>
+    </Card>
+
+    <Card>
+      <Card.Header title="Pickering"></Card.Header>
+      <Card.Content>
+        Pickering is a small market town ideally situated for the North Yorkshire Moors & Steam Railway, 
+        Flamingoland, York, Whitby, Scarborough and the Yorkshire Coast.
+      </Card.Content>
+      </Card>
+
+    </section>
+
+    <footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      <strong>Karen's Cottages</strong> 
+      <span class='pl-1'>Beautiful Holiday Homes in Pickering, North Yorkshire.</span>
+    </p>
+  </div>
+</footer>
   </div>
 }
 
