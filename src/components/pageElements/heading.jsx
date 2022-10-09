0bed0bcb3343cdea552455ba35b3cc41f6fe424b
Mark Fee <git@markfee.com>
2022-10-09 11:13:38 +0100

    Added contact details to header

--- src/components/pageElements/heading.jsx
+++ src/components/pageElements/heading.jsx
@@ -1,9 +1,31 @@
 
 export const Heading = () => <section className="hero is-small is-primary">
-<div className="hero-body">
-  <div className="container">
-    <h1 className="title"> Karen's Cottages </h1>
-    <h2 className="subtitle"> Beautiful Holiday Homes in Pickering, North Yorkshire. </h2>
+  <div className="hero-body">
+    <div className="container">
+      <div className="columns">
+       <div className="column">
+          <h1 className="title"> Karen's Cottages </h1>
+          <h2 className="subtitle"> Beautiful Holiday Homes in Pickering, North Yorkshire. </h2>
+        </div>
+        <div className="column">
+          <div className="contact">
+            <div className="mb-1">
+              <span className="mr-2">Telephone:</span>	
+              <span className="">01751 473258</span>
+            </div>
+            <div className="mb-1">
+              <span className="mr-2">Email:</span>		
+              <span className="">
+                <a href="mailto:hill66@talktalk.net?Subject=Karen's Cottages Availability.">hill66@talktalk.net</a>
+              </span>
+            </div>
+            <div className="mb-1">
+              <span className="mr-2">Website:</span>
+              <span className=""><a href="http://www.karenscottages.co.uk">karenscottages.co.uk</a></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-</div>
 </section>
