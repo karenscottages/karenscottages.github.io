a4d3615dc438ca5a089cf840f5e6b0ae61cf5f24
karenscottages <104299442+karenscottages@users.noreply.github.com>
2022-10-30 10:31:03 +0000

    Deploying to gh-pages from @ karenscottages/karenscottages.github.io@bbc369bd20771b7336e81e74ccffbe64dfcf1d13 🚀

--- _next/static/1bHbMEiQFmrDt0DS6ipgo/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4b86149a4c66e08f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
